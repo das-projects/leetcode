929e7a68a0955d07f59c720d1c40d6a8e2ed968b
Arijit Das <arijit.das@selfsupervised.de>
2024-09-02 22:05:34 +0200

    21. Merge Two Sorted Lists

--- linked_list/merge_sorted_lists.py
+++ linked_list/merge_sorted_lists.py
@@ -22,8 +22,8 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode()
-        current = dummy
+        head = ListNode()
+        current = head
         while l1 and l2:
             if l1.val < l2.val:
                 current.next = l1
@@ -33,4 +33,4 @@ class Solution:
                 l2 = l2.next
             current = current.next
         current.next = l1 or l2
-        return dummy.next
+        return head.next
